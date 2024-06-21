7a647468493d159afe3266ba106096a8008e3e49
welsir <1824379011@qq.com>
2024-06-21 23:06:16 +0800

    项目初始化

---
+++ Mixi-common/src/main/java/com/mixi/common/MixiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.mixi.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixiCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MixiCommonApplication.class, args);
+    }
+
+}
---
+++ Mixi-common/src/test/java/com/mixi/common/MixiCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mixi.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixiCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
