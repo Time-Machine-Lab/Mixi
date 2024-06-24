aaded059527ded23bbc4286a90a76a04501b0354
welsir <1824379011@qq.com>
2024-06-24 10:17:20 +0800

    Netty服务端启动流程框架搭建，根pom做版本控制

--- Mixi-server/src/main/java/com/mixiserver/MixiServerApplication.java
+++ Mixi-server/src/main/java/com/mixiserver/MixiServerApplication.java
@@ -1,13 +1,22 @@
 package com.mixiserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class MixiServerApplication {
+public class MixiServerApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(MixiServerApplication.class, args);
     }
 
+    @Resource
+    NettyServerBoot nettyServerBoot;
+    @Override
+    public void run(String... args) throws Exception {
+        nettyServerBoot.start();
+    }
 }
