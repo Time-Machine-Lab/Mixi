81b4ad3a6d69e47a826f96cce3cdbb388af5552c
Klein <11241686+Klein422@user.noreply.gitee.com>
2024-07-08 14:42:50 +0800

    WebSocket API基本功能封装

--- Mixi-ui/src/router/index.ts
+++ Mixi-ui/src/router/index.ts
@@ -13,6 +13,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/chat',
+      name: 'chat',
+      component: () => import('@/views/demo/chatDemo.vue')
     }
   ]
 })
---
+++ Mixi-ui/src/views/demo/chatDemo.vue
@@ -0,0 +1,22 @@
+<!--
+ * @Author: Dhx
+ * @Date: 2024-07-08 09:17:23
+ * @Description: 
+ * @FilePath: \Mixi\Mixi-ui\src\views\demo\chatDemo.vue
+-->
+<template>
+    <div>
+        123
+    </div>
+</template>
+<script lang="ts">
+import MixiWebSocket from '@/util/webSocket'
+let webSocket = new MixiWebSocket("ws://localhost:8001/websocket-server")
+webSocket.init()
+webSocket.onmessage((event)=>{
+    console.log(event.data)
+})
+webSocket.onopen(()=>{
+    webSocket.send('hello')
+})
+</script>
